87a0be7745eb93b9d555db640619d9148032c2ed
razmik.gharibyan <razmik.gharibyan@sflpro.com>
2021-05-07 15:27:38 +0400

    Minor improvements

--- lib/clear_all_notifications.dart
+++ lib/clear_all_notifications.dart
@@ -5,12 +5,11 @@ import 'package:flutter/services.dart';
 class ClearAllNotifications {
   static const MethodChannel _channel = const MethodChannel('clear_all_notifications');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
   static Future<void> get clear async {
-    await _channel.invokeMethod('clear');
+    try {
+      await _channel.invokeMethod('clear');
+    } catch (e) {
+      throw Exception('Cannot clear notifications');
+    }
   }
 }
