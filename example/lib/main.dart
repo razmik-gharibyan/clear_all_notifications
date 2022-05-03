037184109533ffe2940a9e95022b3c32b102288b
razmik.gharibyan <razmik.gharibyan@sflpro.com>
2022-05-03 19:11:45 +0400

    #comment Upgrade null safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:clear_all_notifications/clear_all_notifications.dart';
 
 void main() {
--- test/clear_all_notifications_test.dart
+++ test/clear_all_notifications_test.dart
@@ -3,21 +3,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:clear_all_notifications/clear_all_notifications.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('clear_all_notifications');
 
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await ClearAllNotifications.platformVersion, '42');
-  });
 }
