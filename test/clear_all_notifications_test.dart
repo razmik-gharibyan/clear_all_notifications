b02184363ae603dfc40b123b8419c7d1df4e4968
Julian Steenbakker <jst@beverinnovations.com>
2023-07-19 09:10:29 +0200

    imp: upgrade gradle, compileSDK version, remove .gitignore files, format code, upgrade other dependencies

--- test/clear_all_notifications_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:clear_all_notifications/clear_all_notifications.dart';
-
-void main() {
-
-}
